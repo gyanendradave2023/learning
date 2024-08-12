4760a09abfc8d7e3eb5cf27c941d0c5ca34eeaa1
Gyanendra Dave <gyanendradave2023@gmail.com>
2024-08-12 21:06:58 +0530

    ui

--- React scaler/backend/class-1/index.js
+++ React scaler/backend/class-1/index.js
@@ -1,5 +1,9 @@
 
 const mongoose = require('mongoose');
+const express = require('express');
+
+const app = express();
+app.use(express.json());
 
 const dbUrl = `mongodb+srv://gyanendradave2023:ZjQStut5cdMdfJmx@cluster0.sjipppy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -36,4 +40,40 @@ const productSchema = new mongoose.Schema({
 {timestamps:true}
 );
 
-const productModel = mongoose.model("products", productSchema);
+const ProductModel = mongoose.model("products", productSchema);
+
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log("Server Started");
+});
+
+
+app.post('/api/products', async (req, res) => {
+    const body = req.body;
+    const product = await ProductModel.create({
+        product_name: body.product_name,
+        product_price: body.product_price,
+        product_qty: body.product_qty,
+        isInStock: body.isInStock,
+        category: body.category,
+    });
+
+    console.log(product);
+    return res.status(201).json({message: 'Product created', product: product});
+});
+
+
+app.get('/api/products', async (req, res) => {
+    const allProducts = await ProductModel.find();
+    console.log(allProducts);
+    return res.status(200).json({message: 'All Products', product: allProducts});
+})
+
+app.get('/api/products/:id', async (req, res) => {
+    const id = req.params.id;
+    const product = await ProductModel.findById(id)
+    console.log(product);
+    return res.status(200).json({message: 'Find Product', product:product});
+})
